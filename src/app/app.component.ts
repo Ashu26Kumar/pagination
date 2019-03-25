802086ad8e63653519388e039ed08d586ced9cfd
Ashutosh Kumar <ashutosh.7585@gmail.com>
2019-03-25 13:01:54 +0000

    pagination done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,62 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges  {
+ 
   title = 'pagination';
+  data:any[]=[{
+    id:1,name:"ashu"
+  },{
+    id:2,name:"ashu"
+  },{
+    id:3,name:"ashu"
+  },{
+    id:4,name:"ashu"
+  },{
+    id:5,name:"ashu"
+  },{
+    id:6,name:"ashu"
+  },{
+    id:7,name:"ashu"
+  },
+  {
+    id:8,name:"ashu"
+  },{
+    id:9,name:"ashu"
+  },{
+    id:10,name:"ashu"
+  },{
+    id:11,name:"ashu"
+  },{
+    id:12,name:"ashu"
+  },];
+   length = this.data.length;
+  pageSize = 5;
+  pageIndex=0;
+  lowValue:Number=0;
+  highValue:Number=this.pageSize;
+  //pageSizeOptions: number[] = [5, 10, 25, 100];
+  
+  getData(event){
+    console.log(event);
+   
+         this.pageSize=event.pageSize;
+       
+     if(event.pageIndex === this.pageIndex + 1){
+        this.lowValue = this.lowValue + this.pageSize;
+        this.highValue =  this.highValue + this.pageSize;
+        
+       }
+    else if(event.pageIndex === this.pageIndex - 1){
+       this.lowValue = this.lowValue - this.pageSize;
+       this.highValue =  this.highValue - this.pageSize;
+       
+      }   
+      
+       
+       this.pageIndex = event.pageIndex;
+  }
+  
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
+import {MatCardModule,MatSelectModule,MatFormFieldModule} from "@angular/material"
 import { AppComponent } from './app.component';
+import {NgxPaginationModule} from "ngx-pagination";
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+// import {MatSelectModule} from '@angular/material/select';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatCardModule,NgxPaginationModule,MatSelectModule,MatFormFieldModule,MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
